637b59233d7b2d49a467917c5754c42aca75cce0
1051795273 <1051795273@qq.com>
2018-03-20 16:21:54 +0800

    后台欢迎页代码、文章商户功能代码

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -24,7 +24,6 @@ Class AuthorController extends Controller{
         $post = $request -> input();
         if (Auth::attempt(['email' => $post['email'], 'password' => $post['password']])) {
             $user = Auth::user();
-            var_dump($user);
 
             $data = [
                 'status' => 200,
